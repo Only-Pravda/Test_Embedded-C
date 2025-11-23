2484ebd18364a88850b05a849156f84aaa5d368b
Only-Pravda <pravda@example.com>
2025-11-23 15:59:44 +0700

    Добавил проверку на отрицательные числа

--- Bit_operation_1.c
+++ Bit_operation_1.c
@@ -1,16 +1,39 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 int main(void) {
     printf("Hello, world!\n");
     unsigned int n;
     printf("Введите целое положительное число: ");
     fflush(stdout);
-    
-    char after_num;
-    if (scanf("%u%c", &n, &after_num) != 2 || after_num != '\n') {
+
+    char buf[64];
+    if (!fgets(buf, sizeof(buf), stdin)) {
+        printf("Ошибка ввода!\n");
+        return 1;ы
+    }
+    // Удалим перевод строки в конце, если есть
+    size_t len = strlen(buf);
+    if (len > 0 && buf[len - 1] == '\n') buf[len - 1] = '\0';
+
+    // Парсим как long, чтобы проверить знак и переполнение
+    char *endptr = NULL;
+    long tmp = strtol(buf, &endptr, 10);
+    if (endptr == buf || *endptr != '\0') {
         printf("Ошибка: введите целое положительное число без десятичных точек!\n");
         return 1;
     }
+    if (tmp < 0) {
+        printf("Ошибка: введите положительное число (без знака '-')!\n");
+        return 1;
+    }
+    if ((unsigned long)tmp > UINT_MAX) {
+        printf("Ошибка: число слишком большое!\n");
+        return 1;
+    }
+    n = (unsigned int)tmp;
     int bits = sizeof(n) * 8;
     int started = 0;
     printf("Двоичное представление: ");
